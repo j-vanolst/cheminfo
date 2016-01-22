6f2efe2c0255388a2ad6642318e3219eca436d9c
j-vanolst <vanolstj@gmail.com>
2016-01-22 20:58:39 +1300

    Fourth Commit

--- cheminfo/cheminfo/Program.cs
+++ cheminfo/cheminfo/Program.cs
@@ -10,6 +10,11 @@ namespace cheminfo
     class Program
     {
         SQLiteConnection MainDatabase;
+
+        public static List<double> ElementNumbers = new List<double>();
+        public static List<string> ElementStrings = new List<string>();
+        public static List<string> ClassInstances = new List<string>();
+
         static void Main(string[] args)
         {
             Program p = new Program();
@@ -19,7 +24,8 @@ namespace cheminfo
             Console.Title = "ChemInfo";
             Console.WriteLine("CTRL + C To Quit.");
             ConnectToDatabase();
-            DBSchema();
+            GetClassInstances();
+            ReadDatabase();
             Console.ReadKey();
         }
         //Initiates the connect to the SQLite database file
@@ -28,7 +34,18 @@ namespace cheminfo
             MainDatabase = new SQLiteConnection("Data Source=Elements.db;Version=3;");
             MainDatabase.Open();
         }
-        public void DBSchema()
+        void GetClassInstances()
+        {
+            string sql = "select Symbol from BaseInfo";
+            SQLiteCommand command = new SQLiteCommand(sql, MainDatabase);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                string Symbol = Convert.ToString(reader["Symbol"]);
+                ClassInstances.Add(Symbol);
+            }
+        }
+        void ReadDatabase()
         {
             string sql = "select * from BaseInfo";
             SQLiteCommand command = new SQLiteCommand(sql, MainDatabase);
@@ -39,33 +56,51 @@ namespace cheminfo
                 string Name, Symbol, Series, State;
 
                 //Converts database objects to usable formats (strings, ints)
-                AtomicNumber = Convert.ToInt32(reader["AtomicNumber"]);
+                AtomicNumber = Convert.ToDouble(reader["AtomicNumber"]);
+                AtomicMass = Convert.ToDouble(reader["AtomicMass"]);
+                MeltingPoint = Convert.ToDouble(reader["MeltingPoint"]);
+                BoilingPoint = Convert.ToDouble(reader["BoilingPoint"]);
+                Ionization = Convert.ToDouble(reader["Ionization"]);
+                Radius = Convert.ToDouble(reader["Radius"]);
+                Density = Convert.ToDouble(reader["Density"]);
+                YearDiscovered = Convert.ToDouble(reader["YearDiscovered"]);
                 Name = Convert.ToString(reader["Name"]);
                 Symbol = Convert.ToString(reader["Symbol"]);
+                Series = Convert.ToString(reader["Series"]);
                 State = Convert.ToString(reader["State"]);
                 //Console.WriteLine("Name: {0} Symbol: {1} State: {2} Atomic Number: {3}", Name, Symbol, State, AtomicNumber);
 
-                List<string> ElementStrings = new List<string>();
+                //Appends the properties from the database to an array
                 ElementStrings.Add(Name);
                 ElementStrings.Add(Symbol);
+                ElementStrings.Add(Series);
                 ElementStrings.Add(State);
 
-                List<double> ElementNumbers = new List<double>();
-                ElementInts.Add(AtomicNumber);
-
-                Console.WriteLine(ElementStrings[0] + ElementStrings[1] + ElementStrings[2]);
-                Console.WriteLine(ElementNumebers[0]);
-                
+                ElementNumbers.Add(AtomicNumber);
+                ElementNumbers.Add(AtomicMass);
+                ElementNumbers.Add(MeltingPoint);
+                ElementNumbers.Add(BoilingPoint);
+                ElementNumbers.Add(Ionization);
+                ElementNumbers.Add(Radius);
+                ElementNumbers.Add(Density);
+                ElementNumbers.Add(YearDiscovered);
             }
         }
-    }
-    class Chemical
+        public List<string> GetStringList()
+        {
+            return ElementStrings;
+        }
+        public List<double> GetNumberList()
+        {
+            return ElementNumbers;
+        }
+        }
+    class Element
     {
         double AtomicNumber, AtomicMass, MeltingPoint, BoilingPoint, Ionization, Radius, Density, YearDiscovered;
         string Name, Symbol, Series, State;
         public void SetAttributes()
         {
-
         }
     }
 }
