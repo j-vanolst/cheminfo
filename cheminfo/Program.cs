c395175100fa335461c84c2cfc853e26ef60e501
j-vanolst <vanolstj@gmail.com>
2016-01-22 17:25:45 +1300

    Initial Commit

---
+++ cheminfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace cheminfo
+{
+    class Program
+    {
+        SQLiteConnection m_dbConnection;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+        public Program()
+        {
+            ConnectToDatabase();
+            DisplayAllRecords();
+        }
+        void ConnectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=ChemicalInformation.db;Version=3;");
+            m_dbConnection.Open();
+        }
+        void DisplayAllRecords()
+        {
+            string sql = "select * from BaseInformation";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+        }
+
+
+        }
+}
